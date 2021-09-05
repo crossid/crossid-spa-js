9198f4d1b69ef528175eb7d7eec92d5cc619c91b
Asaf Shakarzy <asaf@crossid.io>
2021-09-05 14:51:43 +0300

    fix: correct oauth endpoints.

--- test/client/const.ts
+++ test/client/const.ts
@@ -1,17 +1,15 @@
-export const TEST_ISSUER = 'https://myorg.crossid.io/oauth2/default/'
+export const TEST_ISSUER = 'https://myorg.crossid.io/oauth2/'
 
 export const TEST_AUDIENCE = ['myorg.com']
 
 export const TEST_SCOPE = 'openid'
 
 export const TEST_AUTHORIZATION_ENDPOINT =
-  'https://myorg.crossid.io/oauth2/default/auth'
+  'https://myorg.crossid.io/oauth2/auth'
 
-export const TEST_TOKEN_ENDPOINT =
-  'https://myorg.crossid.io/oauth2/default/token'
+export const TEST_TOKEN_ENDPOINT = 'https://myorg.crossid.io/oauth2/token'
 
-export const TEST_LOGOUT_ENDPOINT =
-  'https://myorg.crossid.io/oauth2/default/logout'
+export const TEST_LOGOUT_ENDPOINT = 'https://myorg.crossid.io/oauth2/logout'
 
 export const TEST_CLIENT_ID = 'client1'
 
