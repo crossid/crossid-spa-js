01243876600caa8f4c0c03406a82521d471e776a
cicd <cicd@crossid.io>
2021-12-26 15:00:20 +0200

    fix: url encoding for arrays
    Authorization url expects audience to be separated by space rather comma.

--- src/url.ts
+++ src/url.ts
@@ -2,6 +2,11 @@
 export const createQueryString = (qp: object) => {
   return Object.keys(qp)
     .filter((k) => qp[k] !== undefined && qp[k] !== null)
-    .map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(qp[k]))
+    .map(
+      (k) =>
+        encodeURIComponent(k) +
+        '=' +
+        encodeURIComponent(Array.isArray(qp[k]) ? qp[k].join(' ') : qp[k])
+    )
     .join('&')
 }
--- test/url.test.ts
+++ test/url.test.ts
@@ -6,10 +6,13 @@ describe('createQueryString', () => {
       createQueryString({
         s: 'foo',
         u: 'https://bar.baz',
+        ua: ['https://bar.baz1', 'https://bar.baz2'],
         n: 1,
         un: undefined,
         nu: null,
       })
-    ).toBe('s=foo&u=https%3A%2F%2Fbar.baz&n=1')
+    ).toBe(
+      's=foo&u=https%3A%2F%2Fbar.baz&ua=https%3A%2F%2Fbar.baz1%20https%3A%2F%2Fbar.baz2&n=1'
+    )
   })
 })
